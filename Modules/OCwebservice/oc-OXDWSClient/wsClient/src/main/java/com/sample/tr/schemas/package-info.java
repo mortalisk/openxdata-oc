a9821d3d213d57464f1890c80b012624c89d3a3f
jorn.klungsoyr@cih.uib.no <jorn.klungsoyr@cih.uib.no@6cddc80d-1c40-44cd-927a-2be6005847d2>
2011-05-10 13:55:54 +0000

    Moving these unmaintained projects to trash. When OpenClinica integration work is restarted, then some of this code can be reused and/or be used as a reference for some of the key technical issues.
    git-svn-id: https://svn.openxdata.org/Server/trunk@4321 6cddc80d-1c40-44cd-927a-2be6005847d2

--- Modules/OCwebservice/oc-OXDWSClient/wsClient/src/main/java/com/sample/tr/schemas/package-info.java
+++
@@ -1,3 +0,0 @@
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://sample.com/tr/schemas", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.sample.tr.schemas;
--- Modules/workflows/SliverSE/src/main/old/MyService.java
+++
@@ -1,9 +0,0 @@
-package main.old;
-
-public class MyService {
-	
-	public static MyCustomType someService(){
-		
-		return new MyCustomType();
-	}
-}
