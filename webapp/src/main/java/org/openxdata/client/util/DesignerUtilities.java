819327c0b956a80ab69df6387957987fbaf65c83
cattabanks@gmail.com <cattabanks@gmail.com@6cddc80d-1c40-44cd-927a-2be6005847d2>
2011-05-18 14:21:38 +0000

    DesignerUtilities now handles Item export by building the url that connects to the servlet which downloads the item.
    git-svn-id: https://svn.openxdata.org/Server/trunk@4405 6cddc80d-1c40-44cd-927a-2be6005847d2

--- webapp/src/main/java/org/openxdata/client/util/DesignerUtilities.java
+++ webapp/src/main/java/org/openxdata/client/util/DesignerUtilities.java
@@ -1,8 +1,18 @@
 package org.openxdata.client.util;
 
+import org.openxdata.client.AppMessages;
+import org.openxdata.server.admin.model.Editable;
 import org.openxdata.server.admin.model.FormDef;
 import org.openxdata.server.admin.model.FormDefVersion;
+import org.openxdata.server.admin.model.StudyDef;
 import org.purc.purcforms.client.util.FormDesignerUtil;
+
+import com.extjs.gxt.ui.client.widget.MessageBox;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.user.client.Window;
 
 /**
  * Encapsulates utility functions used by the PurcForms Designer.
@@ -10,6 +20,8 @@ import org.purc.purcforms.client.util.FormDesignerUtil;
  */
 public class DesignerUtilities {
 
+	static AppMessages appMessages = GWT.create(AppMessages.class);
+	
 	/**
 	 * Creates a unique form binding.
 	 * 
@@ -21,5 +33,48 @@ public class DesignerUtilities {
 		FormDef formDef = formDefVersion.getFormDef();
 		String binding = formDef.getStudy().getName() + "_" + formDef.getName() + "_" + formDefVersion.getName();
 		return FormDesignerUtil.getXmlTagName(binding);
+	}
+	
+	/**
+	 * Exports the given Editable. Right now we only export studies.
+	 * 
+	 * @param editable Editable to export.
+	 * @param fileName File Name for the item to export.
+	 */
+	public static void exportEditable(final Editable editable, final String fileName){
+
+		ProgressIndicator.showProgressBar();
+		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+			@Override
+			public void execute() {
+				try{
+					Integer id = null;
+					String type = "study";
+					if(editable instanceof FormDef){
+						type = "form";
+						id = ((FormDef)editable).getFormId();
+					}
+					else if(editable instanceof FormDefVersion){
+						type = "version";
+						id = ((FormDefVersion)editable).getFormDefVersionId();
+					}
+					else
+						id = ((StudyDef)editable).getStudyId();
+
+					String url = "studyexport?";
+					url += "type=" + type;
+					url += "&id=" + id;
+					url += "&filename=" + fileName;
+
+					Window.Location.replace(URL.encode(url));
+
+					ProgressIndicator.hideProgressBar();	
+				}
+				catch(Exception ex){
+					MessageBox.alert(appMessages.error(), appMessages.exportError(), null);
+					ProgressIndicator.hideProgressBar();	
+				}	
+			}
+		});
 	}
 }
