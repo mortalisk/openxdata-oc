76c36b47c8d4316da8c7caf3725f8404def841bf
cattabanks@gmail.com <cattabanks@gmail.com@6cddc80d-1c40-44cd-927a-2be6005847d2>
2011-05-19 11:49:48 +0000

    Added internalization class and properties file for openxdata module. For some reason, the one in EMIT cannot work here because it will cause a cyclic redundancy check.
    git-svn-id: https://svn.openxdata.org/Server/trunk@4467 6cddc80d-1c40-44cd-927a-2be6005847d2

---
+++ webapp/src/main/java/org/openxdata/server/admin/client/OpenXDataAppMessages.java
@@ -0,0 +1,38 @@
+package org.openxdata.server.admin.client;
+
+import com.google.gwt.i18n.client.Messages;
+
+public interface OpenXDataAppMessages extends Messages {
+	
+	String datasets();
+	
+	String addAnotherDatasetGroup();
+	
+	String newDatasetGroup();
+	
+	String pleaseAddDatasetGroup();
+	
+	String newDataset();
+	
+	String selectItemDelete();
+	
+	String deleteConfirmation();
+	
+	String deleteDataset();
+	
+	String noPrivilegesDeleteDatasets();
+
+	String noPrivilegesAddDatasets();
+	
+	String addDatasetGroup();
+	
+	String deleteDatasetGroup();
+	
+	String addDataset();
+	
+	String formSource();
+	
+	String datasetFields();
+	
+	String formHasNoQuestions();
+}
