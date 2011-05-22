989e2f2c669019a55c5d96ca3eff133e5e348ed5
cattabanks@gmail.com <cattabanks@gmail.com@6cddc80d-1c40-44cd-927a-2be6005847d2>
2011-05-22 09:26:58 +0000

    Re organising methods in this class for better flow
    git-svn-id: https://svn.openxdata.org/Server/trunk@4519 6cddc80d-1c40-44cd-927a-2be6005847d2

--- webapp/src/main/java/org/openxdata/client/EmitAsyncCallback.java
+++ webapp/src/main/java/org/openxdata/client/EmitAsyncCallback.java
@@ -19,14 +19,6 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public abstract class EmitAsyncCallback<T> implements AsyncCallback<T> {
     
     AppMessages appMessages = GWT.create(AppMessages.class);
-    
-    /**
-     * Method to override if there is some custom post exception handling work to be done.
-     * Called after client has been notified of errors.
-     * @param throwable
-     */
-    public void onFailurePostProcessing(Throwable throwable) {
-    }
 
     /**
      * Implements some auto error handling for SpringSecurityException and general errors.
@@ -36,16 +28,22 @@ public abstract class EmitAsyncCallback<T> implements AsyncCallback<T> {
         GWT.log("Error caught while performing an action on the server: "+throwable.getMessage(), throwable);
         if (throwable instanceof OpenXDataSessionExpiredException) {
             // allow the user to login again (show a login popup so they can continue where they left off)
-            Dispatcher.get().dispatch(LoginController.LOGIN);
+            Dispatcher.get().dispatch(LoginController.SESSION_TIMEOUT);
         } else if (throwable instanceof OpenXDataSecurityException) {
             // access denied
             MessageBox.alert(appMessages.error(), appMessages.accessDeniedError(), null);
         } else {
             // all other errors
-            MessageBox.alert(appMessages.error(), appMessages.pleaseTryAgainLater(throwable.getMessage()), null);
+        	onFailurePostProcessing(throwable);
         }
-        onFailurePostProcessing(throwable);
     }
     
-
+    /**
+     * Method to override if there is some custom post exception handling work to be done.
+     * Called after client has been notified of errors.
+     * @param throwable
+     */
+    public void onFailurePostProcessing(Throwable throwable) {
+    	MessageBox.alert(appMessages.error(), appMessages.pleaseTryAgainLater(throwable.getMessage()), null);
+    }
 }
