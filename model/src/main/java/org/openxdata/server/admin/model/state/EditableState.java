ec03c77ad357fbdb551251dcbf15db7ebcbe16d8
cattabanks@gmail.com <cattabanks@gmail.com@6cddc80d-1c40-44cd-927a-2be6005847d2>
2011-05-26 17:42:20 +0000

    Added the state enum object to the model package
    git-svn-id: https://svn.openxdata.org/Server/trunk@4607 6cddc80d-1c40-44cd-927a-2be6005847d2

---
+++ model/src/main/java/org/openxdata/server/admin/model/state/EditableState.java
@@ -0,0 +1,15 @@
+package org.openxdata.server.admin.model.state;
+
+/**
+ * Models the Editable's possible state transitions. 
+ *
+ */
+public enum EditableState {
+	
+	/** State for an Editable that was successfully loaded.*/
+	LOADED,
+	
+	/** State for an Editable that has data. */
+	HASDATA
+
+}
