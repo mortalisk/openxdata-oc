2a68aa326337e409825db83138a5b155d69abe10
cattabanks@gmail.com <cattabanks@gmail.com@6cddc80d-1c40-44cd-927a-2be6005847d2>
2011-08-01 08:48:33 +0000

    Formatted this class.
    git-svn-id: https://svn.openxdata.org/Server/trunk@4932 6cddc80d-1c40-44cd-927a-2be6005847d2

--- webapp/src/main/java/org/openxdata/client/model/FormSummary.java
+++ webapp/src/main/java/org/openxdata/client/model/FormSummary.java
@@ -42,131 +42,128 @@ public class FormSummary extends BaseModel {
 	    setResponses(responses);
 	}
 
-	  public String getId() {
-	    return get("id");
-	  }
+	public String getId() {
+		return get("id");
+	}
 
-	  public void setId(String id) {
-	    set("id", id);
-	  }	
+	public void setId(String id) {
+		set("id", id);
+	}
 
-	  public String getForm() {
-	    return get("form");
-	  }
+	public String getForm() {
+		return get("form");
+	}
 
-	  public void setForm(String form) {
-	    set("form", form);
-	  }
+	public void setForm(String form) {
+		set("form", form);
+	}
 
-	  public String getStatus() {
-	    return get("status");
-	  }
+	public String getStatus() {
+		return get("status");
+	}
 
-	  public void setStatus(String status) {
-	    set("status", status);
-	  }
+	public void setStatus(String status) {
+		set("status", status);
+	}
+
+	public String getOrganisation() {
+		return get("organisation");
+	}
+
+	public void setOrganisation(String organisation) {
+		set("organisation", organisation);
+	}
+
+	public String getCreator() {
+		return get("creator");
+	}
+
+	public void setCreator(String creator) {
+		set("creator", creator);
+	}
+
+	public Date getChanged() {
+		return get("changed");
+	}
+
+	public void setChanged(Date changed) {
+		set("changed", changed);
+	}
+
+	public String getResponses() {
+		return get("responses");
+	}
+
+	public void setResponses(String responses) {
+		set("responses", responses);
+	}
+
+	public String getVersion() {
+		return get("version");
+	}
+
+	public void setVersion(String version) {
+		set("version", version);
+	}
 	  
+	public void setPublished(Boolean published) {
+		set("published", published);
+	}
 
-	  public String getOrganisation() {
-	    return get("organisation");
-	  }
+	public Boolean isPublished() {
+		return get("published");
+	}
 
-	  public void setOrganisation(String organisation) {
-	    set("organisation", organisation);
-	  }
-	  
+	public FormDef getFormDefinition() {
+		return formDef;
+	}
 
-	  public String getCreator() {
-	    return get("creator");
-	  }
+	public FormDefVersion getFormVersion() {
+		return formVersion;
+	}
 
-	  public void setCreator(String creator) {
-	    set("creator", creator);
-	  }
-	  
+	public void setFormVersion(FormDefVersion formVersion) {
+		this.formVersion = formVersion;
+		this.formDef = formVersion.getFormDef();
+		updateFormVersion(formVersion);
+	}
 
-	  public Date getChanged() {
-	    return get("changed");
-	  }
+	public void setFormDef(FormDef formDef) {
+		this.formDef = formDef;
+		updateFormDefinition(formDef);
+	}
 
-	  public void setChanged(Date changed) {
-	    set("changed", changed);
-	  }
-	  
-	  public String getResponses() {
-	      return get("responses");
-	  }
-	  
-	  public void setResponses(String responses) {
-	      set("responses", responses);
-	  }
-	  
-	  public String getVersion() {
-	      return get("version");
-	  }
-	  
-	  public void setVersion(String version) {
-	      set("version", version);
-	  }
-	  
-	  public void setPublished(Boolean published) {
-		  set("published", published);
-	  }
-	  
-	  public Boolean isPublished() {
-		  return get("published");
-	  }
+	public void updateFormDefinition(FormDef formDef) {
+		setId(String.valueOf("d" + formDef.getId()));
+		setForm(formDef.getName());
+		// newly created forms may not have any version
+		// FIXME: what is this about no versions....
+		if (formDef.getVersions().size() > 0) {
+			FormDefVersion formVersion = formDef.getDefaultVersion();
+			setVersion(formVersion.getName());
+			setPublished(true);
+		} else {
+			setPublished(false);
+		}
+		StudyDef study = formDef.getStudy();
+		setOrganisation(study.getName());
+		setCreator(formDef.getCreator().getName());
+		if (formDef.getDateChanged() == null) {
+			setChanged(formDef.getDateCreated());
+		}
+	}
 
-    public FormDef getFormDefinition() {
-        return formDef;
-    }
-    
-    public FormDefVersion getFormVersion() {
-    	return formVersion;
-    }
-
-    public void setFormVersion(FormDefVersion formVersion) {
-        this.formVersion = formVersion;
-        this.formDef = formVersion.getFormDef();
-        updateFormVersion(formVersion);
-    }
-    
-    public void setFormDef(FormDef formDef) {
-    	this.formDef = formDef;
-    	updateFormDefinition(formDef);
-    }
-    
-    public void updateFormDefinition(FormDef formDef) {
-		setId(String.valueOf("d"+formDef.getId()));
-        setForm(formDef.getName());
-        //newly created forms may not have any version
-        // FIXME: what is this about no versions....
-        if (formDef.getVersions().size() > 0) {
-        	FormDefVersion formVersion = formDef.getDefaultVersion();
-        	setVersion(formVersion.getName());
-        	setPublished(true);
-        } else {
-        	setPublished(false);
-        }
-        StudyDef study = formDef.getStudy();
-        setOrganisation(study.getName());
-        setCreator(formDef.getCreator().getName());
-        if (formDef.getDateChanged() == null) {
-            setChanged(formDef.getDateCreated());
-        }
-    }
-    
-    public void updateFormVersion(FormDefVersion formVersion) {
+	public void updateFormVersion(FormDefVersion formVersion) {
 		setId(String.valueOf(formVersion.getId()));
 		FormDef formDef = formVersion.getFormDef();
-        setForm(formDef.getName());
-        setVersion(formVersion.getName());
-        setPublished(formVersion.getIsDefault());
-        StudyDef study = formDef.getStudy();
-        setOrganisation(study.getName());
-        setCreator(formDef.getCreator().getName());
-        if (formDef.getDateChanged() == null) {
-            setChanged(formDef.getDateCreated());
-        }
+		setForm(formDef.getName());
+		setVersion(formVersion.getName());
+		setPublished(formVersion.getIsDefault());
+		StudyDef study = formDef.getStudy();
+		setOrganisation(study.getName());
+		setCreator(formDef.getCreator().getName());
+		if (formDef.getDateChanged() == null) {
+			setChanged(formDef.getDateCreated());
+		}
 	}
 }
