17125f18729a8d816eb44d65565382d86639d5c4
cattabanks@gmail.com <cattabanks@gmail.com@6cddc80d-1c40-44cd-927a-2be6005847d2>
2011-09-30 09:18:40 +0000

    Fixed #485
    git-svn-id: https://svn.openxdata.org/Server/trunk@5131 6cddc80d-1c40-44cd-927a-2be6005847d2

--- webapp/src/main/java/org/openxdata/server/admin/client/util/StudyImport.java
+++ webapp/src/main/java/org/openxdata/server/admin/client/util/StudyImport.java
@@ -1,6 +1,5 @@
 package org.openxdata.server.admin.client.util;
 
-import org.openxdata.oc.client.convert.ODMImport;
 import org.openxdata.server.admin.model.Editable;
 import org.openxdata.server.admin.model.FormDef;
 import org.openxdata.server.admin.model.FormDefVersion;
@@ -51,9 +50,6 @@ public class StudyImport {
 		
 		Element root = doc.getDocumentElement();
 		if(root != null){
-			
-			if(root.getNodeName().equalsIgnoreCase("ODM"))
-				return ODMImport.importStudyItem(doc);
 			
 			String name = root.getAttribute("name");
 			String description = root.getAttribute("description");
