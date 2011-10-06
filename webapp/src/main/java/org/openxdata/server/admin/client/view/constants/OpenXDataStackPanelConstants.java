46dbad9dba361217757e2e4b5daca11435fd6463
dagmar <dagmar@6cddc80d-1c40-44cd-927a-2be6005847d2>
2011-10-06 09:03:50 +0000

    #471 fixed a bug in the admin UI where the wrong type of data was being displayed on the right hand side
    git-svn-id: https://svn.openxdata.org/Server/trunk@5157 6cddc80d-1c40-44cd-927a-2be6005847d2

--- webapp/src/main/java/org/openxdata/server/admin/client/view/constants/OpenXDataStackPanelConstants.java
+++ webapp/src/main/java/org/openxdata/server/admin/client/view/constants/OpenXDataStackPanelConstants.java
@@ -8,15 +8,15 @@ package org.openxdata.server.admin.client.view.constants;
 public class OpenXDataStackPanelConstants {
 
 	/** The stack panel index for roles. */
-	public static final int INDEX_ROLES = 1;
+	public static final int INDEX_ROLES = 0;
 
 	/** The stack panel index for tasks. */
-	public static final int INDEX_TASKS = 2;
+	public static final int INDEX_TASKS = 1;
 
 	/** The stack panel index for settings. */
-	public static final int INDEX_SETTINGS = 3;
+	public static final int INDEX_SETTINGS = 2;
 
 	/** The stack panel index for reports. */
-	public static final int INDEX_REPORTS = 4;
+	public static final int INDEX_REPORTS = 3;
 	
 }
