80190d42d40fd5b4659df62ef5c8b9d851413c7a
kata mapkon <cattabanks@gmail.com>
2011-11-02 15:44:23 +0100

    Refactored tests. Added test for getStudySubjects()

--- server/src/test/java/org/openxdata/server/service/impl/OpenClinicaServiceImplTest.java
+++ server/src/test/java/org/openxdata/server/service/impl/OpenClinicaServiceImplTest.java
@@ -2,7 +2,6 @@ package org.openxdata.server.service.impl;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -37,11 +36,48 @@ public class OpenClinicaServiceImplTest {
     
     @InjectMocks private OpenclinicaServiceImpl openClinicaService = new OpenclinicaServiceImpl();
 
+    List<String> subjects = new ArrayList<String>();
+    List<StudyDef> studies = new ArrayList<StudyDef>();
     List<ConvertedOpenclinicaStudy> openClinicaConvertedStudies = new ArrayList<ConvertedOpenclinicaStudy>();
 	
     @Before public void setUp() {
     	
-    	ConvertedOpenclinicaStudy convertedStudy = new ConvertedOpenclinicaStudy();
+    	initSubjects();
+    	initStudyDefinitions();
+    	initConvertedOpenClinicaStudies();
+								
+		Mockito.when(studyDAO.getStudies()).thenReturn(studies);
+    	Mockito.when(studyService.getStudyKey(Mockito.anyInt())).thenReturn("key");
+    	Mockito.when(studyDAO.getStudy(Mockito.anyString())).thenReturn(new StudyDef());
+    	Mockito.when(editableDAO.hasEditableData(Mockito.any(Editable.class))).thenReturn(Boolean.TRUE);
+    	
+    	Mockito.when(openClinicaSoapClient.listAll()).thenReturn(openClinicaConvertedStudies);
+    	Mockito.when(openClinicaSoapClient.getSubjectKeys(Mockito.anyString())).thenReturn(subjects);
+    	
+    	openClinicaService.setStudyDAO(studyDAO);
+    	openClinicaService.setEditableDAO(editableDAO);
+				
+    }
+
+	private void initSubjects() {
+		subjects.add("J¿rn");
+		subjects.add("Janne");
+		subjects.add("Morten");
+		subjects.add("Jonny");
+	}
+	
+	private void initStudyDefinitions() {
+		// Study definitions
+		StudyDef study = new StudyDef();
+		study.setName("study");
+		study.setStudyKey("oid");
+		
+		studies.add(study);
+		
+	}
+
+	private void initConvertedOpenClinicaStudies() {
+		ConvertedOpenclinicaStudy convertedStudy = new ConvertedOpenclinicaStudy();
 		convertedStudy.setIdentifier("id");
 		convertedStudy.setOID("oid");
 		convertedStudy.setName("study");
@@ -53,37 +89,14 @@ public class OpenClinicaServiceImplTest {
 		
 		openClinicaConvertedStudies.add(convertedStudy);
 		openClinicaConvertedStudies.add(convertedStudy2);
-				
-		List<StudyDef> studies = new ArrayList<StudyDef>();
-		StudyDef study = new StudyDef();
-		study.setName("nameX");
-		study.setStudyKey("keyX");
-		
-		StudyDef study2 = new StudyDef();
-		study2.setName("nameX2");
-		study2.setStudyKey("keyX2");
-		
-		studies.add(study);
-		studies.add(study2);
-				
-		Mockito.when(studyDAO.getStudies()).thenReturn(studies);
-    	Mockito.when(studyService.getStudyKey(Mockito.anyInt())).thenReturn("key");
-    	Mockito.when(studyDAO.getStudy(Mockito.anyString())).thenReturn(new StudyDef());
-    	Mockito.when(editableDAO.hasEditableData(Mockito.any(Editable.class))).thenReturn(true);
-    	
-    	Mockito.when(openClinicaSoapClient.listAll()).thenReturn(openClinicaConvertedStudies);
-    	
-    	openClinicaService.setStudyDAO(studyDAO);
-    	openClinicaService.setEditableDAO(editableDAO);
-				
-    }
+	}
     
 	@Test public void testHasStudyData(){
 		
 		String studyKey = studyService.getStudyKey(1);
 		assertTrue(openClinicaService.hasStudyData(studyKey));
 		
-		Mockito.when(editableDAO.hasEditableData(Mockito.any(Editable.class))).thenReturn(false);
+		Mockito.when(editableDAO.hasEditableData(Mockito.any(Editable.class))).thenReturn(Boolean.FALSE);
 		String studyKey2 = studyService.getStudyKey(2);
 		assertFalse(openClinicaService.hasStudyData(studyKey2));
 	}
@@ -93,7 +106,37 @@ public class OpenClinicaServiceImplTest {
 		List<OpenclinicaStudy> studies = openClinicaService.getOpenClinicaStudies();
 		
 		assertEquals(2, studies.size());
-		assertEquals("study2", ocStudies.get(0).getName());
-		assertEquals("study", ocStudies.get(1).getName());
+		assertEquals("study", studies.get(0).getName());
+		assertEquals("study2", studies.get(1).getName());
+	}
+	
+	@Test public void testGetOpenCLinicaStudiesMustNotReturnDuplicateStudies(){
+		
+		List<OpenclinicaStudy> ocStudies = null;
+		
+		StudyDef study2 = new StudyDef();
+		study2.setName("study2");
+		study2.setStudyKey("oid2");
+		
+		studies.add(study2);
+		
+		ocStudies = openClinicaService.getOpenClinicaStudies();
+		assertEquals(2, ocStudies.size());
+		
+	}
+	
+	@Test public void testGetSubjectsShouldNotReturnNull(){
+		
+		List<String> studySubjects = openClinicaService.getStudySubjects("studyOID");
+		
+		assertEquals(4, studySubjects.size());
+		assertEquals("J¿rn", studySubjects.get(0));
+		assertEquals("Janne", studySubjects.get(1));
+		assertEquals("Morten", studySubjects.get(2));
+		assertEquals("Jonny", studySubjects.get(3));
+	}
+	
+	@Test public void testImportOpenClinicaStudyShouldReturnCorrectXML(){
+		
 	}
 }
