ad4c5a28cd769b73c86bd63b64b92c73b813651d
kata mapkon <cattabanks@gmail.com>
2011-11-09 17:27:39 +0100

    Clean up after last round of re-factor

--- server/src/test/java/org/openxdata/server/service/impl/OpenClinicaServiceImplTest.java
+++ server/src/test/java/org/openxdata/server/service/impl/OpenClinicaServiceImplTest.java
@@ -2,12 +2,9 @@ package org.openxdata.server.service.impl;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
