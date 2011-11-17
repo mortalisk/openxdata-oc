f7f8cf1dc12d84be3e20f8c8379f2febb87ba286
kata mapkon <cattabanks@gmail.com>
2011-11-17 10:41:28 +0100

    Really minor clean up.

--- server/src/test/java/org/openxdata/server/service/impl/OpenClinicaServiceImplTest.java
+++ server/src/test/java/org/openxdata/server/service/impl/OpenClinicaServiceImplTest.java
@@ -58,7 +58,6 @@ public class OpenClinicaServiceImplTest {
     	initStudyDefinitions();
     	initConvertedOpenClinicaStudies();
     	
-    	
     	StudyDef study = createStudy();
 				
 		Mockito.when(studyDAO.getStudies()).thenReturn(studies);
